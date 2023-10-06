90e2894933fa3dd751f5ca3c25a49533b9fbfe50
PedroCruz <contatocreed67@gmail.com>
2023-10-06 11:22:00 -0400

    add validations

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,13 +1,23 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace movie_api.Models;
 
 public class Movie
 {
+    [Required(ErrorMessage = "Title is required")]
     public string Title { get; set; }
+
+    [Required(ErrorMessage = "Description is required"))]
+    [MinLength(15, ErrorMessage = "Minimal descriptio is 15 characters")]
     public string Description { get; set; }
+
+    [Required]
+    [MaxLength(60, ErrorMessage = "Exceed the number of characters")]
     public string Gender { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "Minimal Min 70, Max min 600")]
     public int Min { get; set; }
 
 }
