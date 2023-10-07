d2370d407b6dbc31bce8c57546ba32dddf851360
Pedro Cruz <98105642+byPedroCruzDev@users.noreply.github.com>
2023-10-07 17:47:39 -0400

    Atualizar o MovieController.cs
    Add pagination

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using movie_api.Models;
 
 namespace movie_api.Controllers;
@@ -24,9 +24,9 @@ public class MovieController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Movie> GetMovies()
+    public IEnumerable<Movie> GetMovies([FromQuery] int skip = 0, [FromQuery] int take = 20 )
     {
-        return movie;
+        return movie.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
